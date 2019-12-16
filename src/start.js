378f0ccffe57a209fbc5c8b1acbece9138b2027b
Francisco Mendez <pacomendez12@gmail.com>
2019-12-15 23:11:00 -0600

    Editing main menu
    Signed-off-by: Francisco Mendez <pacomendez12@gmail.com>

--- src/start.js
+++ src/start.js
@@ -1,9 +1,9 @@
-const electron = require("electron");
-const app = electron.app;
-const path = require("path");
+const { app, BrowserWindow, Menu } = require("electron");
 const isDev = require("electron-is-dev");
+const path = require("path");
 require("electron-reload");
-const BrowserWindow = electron.BrowserWindow;
+
+const isMac = process.platform === "darwin";
 
 let mainWindow;
 
@@ -23,6 +23,123 @@ function createWindow() {
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
+
+  const template = [
+    // { role: 'appMenu' }
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "services" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideothers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" }
+            ]
+          }
+        ]
+      : []),
+    // { role: 'fileMenu' }
+    {
+      label: "Archivo",
+      submenu: [
+        { label: "Nuevo", accelerator: "ctrl+n" },
+        { label: "Abrir", accelerator: "ctrl+o" },
+        { type: "separator" },
+        { label: "Guardar", accelerator: "ctrl+s" },
+        { label: "Guardar Como...", accelerator: "ctrl+shift+s" },
+        { type: "separator" },
+        { label: "Salir", accelerator: "ctrl+q" }
+        // { role: isMac ? "close" : "quit" }
+      ]
+    },
+    // { role: 'editMenu' }
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(isMac
+          ? [
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Speech",
+                submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }]
+              }
+            ]
+          : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }])
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forcereload" },
+        { role: "toggledevtools" },
+        { type: "separator" },
+        { role: "resetzoom" },
+        { role: "zoomin" },
+        { role: "zoomout" },
+        { type: "separator" },
+        { role: "togglefullscreen" }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(isMac
+          ? [
+              { type: "separator" },
+              { role: "front" },
+              { type: "separator" },
+              { role: "window" }
+            ]
+          : [{ role: "close" }])
+      ]
+    },
+    {
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click: async () => {
+            const { shell } = require("electron");
+            await shell.openExternal("https://electronjs.org");
+          }
+        }
+      ]
+    }
+  ];
+
+  // const template = [
+  //   {
+  //     label: "Archivo",
+  //     submenu: [
+  //       { role: "Abrir" },
+  //       { role: "Guardar" },
+  //       { role: "Guardar como..." },
+  //       { role: "Salir" }
+  //     ]
+  //   }
+  // ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   mainWindow.on("closed", () => {
     mainWindow = null;
